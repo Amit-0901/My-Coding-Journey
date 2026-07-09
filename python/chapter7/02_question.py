959a3e2d316ed81455d88edd9ca6d38987a78bb1
Amit <amitkumar09012006@gmail.com>
2026-07-09 22:46:52 +0530

    python

---
+++ python/chapter7/02_question.py
@@ -0,0 +1,10 @@
+# 2. Write a program to greet all the person names 
+# stored in a list ‘l’ and which starts with S.
+# l = ["Harry", "Soham", "Sachin", "Rahul"]
+
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+for names in l:
+      if (names.startswith("S")):
+       print("Good Day" , names)
+       
+    
---
+++ python/chapter7/04_question.py
@@ -0,0 +1,12 @@
+# 4. Write a program to find whether a given number is prime or not.
+n = int(input("enter the number to check : "))
+if(n < 1):
+    print("number is not prime")
+
+else:
+ for i in range(2,n):
+     if (n%i == 0):
+      print("not prime")
+      break
+ else:
+     print ("prime")
