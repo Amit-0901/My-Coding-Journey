1bec9690449009412b01b2e39c52dec154e4d7e3
Amit <amitkumar09012006@gmail.com>
2026-07-16 19:11:55 +0530

    dsa

---
+++ DSA C++/grid_path.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int grid[m][n];
+        int first =  grid[0][0];
+        int last = grid[m-1][n-1];
+        int count = 0;
+        int i = 0;
+        while(i<(m*n)){
+            
+
+            i++;
+        }
+    }
+};
