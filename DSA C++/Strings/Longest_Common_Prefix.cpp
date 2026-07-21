5e0ba3f30332bdc1620e14c86cf6deb4983682ae
Amit <amitkumar09012006@gmail.com>
2026-07-21 18:02:46 +0530

    ss

--- DSA C++/Strings/Longest_Common_Prefix.cpp
+++ DSA C++/Strings/Longest_Common_Prefix.cpp
@@ -1,5 +1,23 @@
 //Longest Common Prefix
 #include<bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
 
+        string prefix = strs[0];
+
+        for(int i = 1; i < strs.size(); i++) {
+
+            while(strs[i].find(prefix) != 0) {
+                prefix.pop_back();
+
+                if(prefix.empty())
+                    return "";
+            }
+        }
+
+        return prefix;
+    }
+};
 
