015f75a9012ab9969428ab3e1839edcd043f527d
Amit <amitkumar09012006@gmail.com>
2026-07-31 16:57:36 +0530

    dsa

---
+++ DSA C++/Binary Search/12_no_of_rotation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int NumberofRotation(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return low;  
+    }
+};
